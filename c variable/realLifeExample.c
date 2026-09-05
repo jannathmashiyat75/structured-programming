ed2e7e82083bd024974e41cf2ea5917370c275f8
MASHIYAT JANNATH <jannathmashiyat75@gmail.com>
2026-09-05 19:55:26 +0600

    Added c Variables

---
+++ c variable/realLifeExample.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int studentid = 15;
+    int studentage = 23;
+    float studentfee = 70.45;
+    char studentgrade = 'b';
+    printf("students id :%d\n",studentid);
+    printf("students age:%d\n",studentage);
+    printf("students fee:%f\n",studentfee);
+    printf("students grade:%c\n",studentgrade);
+    return 0;
+}
