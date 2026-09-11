474c4b364dc0d02881f48f67fc9481b2c6cc668a
MASHIYAT JANNATH <jannathmashiyat75@gmail.com>
2026-09-11 15:23:14 +0600

    Added boolean

---
+++ c boolean/booleans.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool isprogrammingfun = true;
+    bool fishistasty = false;
+    printf("%d",isprogrammingfun);
+    printf("%d",fishistasty);
+    return 0;
+}
