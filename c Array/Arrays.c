f2660cd84a0e5d9a1d06869d6fc230bab108b86b
MASHIYAT JANNATH <jannathmashiyat75@gmail.com>
2026-09-20 22:43:06 +0600

    Added Array

---
+++ c Array/Arrays.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int mynumber[] = {25,35,34,56};
+    printf("%d",mynumber[0]);
+    return 0;
+}
---
+++ c Array/changeElement.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int mynumber[]= {25,45,67,45,67};
+    mynumber[0] = 33;
+    printf("%d",mynumber[0]);
+    return 0;
+}
